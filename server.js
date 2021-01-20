73a7dfdd6971bf990674b8db26a6dd575a9bbf0c
Victor Volovik <VictorVolovik@users.noreply.github.com>
2021-01-20 12:50:40 +0300

    Fix Redis settings

--- server.js
+++ server.js
@@ -6,15 +6,19 @@ const redis = require("redis");
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const PORT = parseInt(process.env.PORT, 10) || 5000;
+const REDIS_PORT = parseInt(process.env.REDIS_PORT, 10) || 6379;
 
 const API_GET_GITHUB_USER = username => `https://api.github.com/users/${username}`;
 const CACHE_LIFETIME = 1000 * 60 * 5; // 5 minutes
 
 const app = express();
 
-const redisClient = redis.createClient(REDIS_PORT);
+const redisClient = redis.createClient({
+  host: "127.0.0.1", // localhost
+  port: REDIS_PORT
+});
+
 const store = new RedisStore(redisClient);
 
 const apiWithCahe = setup({
